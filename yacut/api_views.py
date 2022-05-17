e431ffd51380a00f6345799c4ebd60eaf3e9c904
Mikhail Kochurov <firepanda70@gmail.com>
2022-05-18 02:53:56 +0500

    Constants for error messages added

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -8,6 +8,13 @@ from .error_handlers import InvalidAPIUsage
 from .models import URL_map
 from .views import get_unique_short_id, is_shortcut_exists
 
+NO_REQUEST_BODY = 'Отсутствует тело запроса'
+NO_REQUIRED_FIELD = '"{field}" является обязательным полем!'
+WRONG_FORMAL = 'Не верный формат ссылки в поле "{field}"'
+WRONG_SHORTCUT_NAME = 'Указано недопустимое имя для короткой ссылки'
+NAME_TAKEN = 'Имя "{shortcut}" уже занято.'
+ID_NOT_FOUND = 'Указанный id не найден'
+
 
 @app.route('/api/id/', methods=['POST'])
 def add_url_map():
@@ -15,15 +22,15 @@ def add_url_map():
     if data is None or '':
         raise InvalidAPIUsage('Отсутствует тело запроса')
     if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!')
+        raise InvalidAPIUsage(NO_REQUIRED_FIELD.format(field='url'))
     if not re.search(URL_REGEXP, data['url']):
-        raise InvalidAPIUsage('Не верный формат ссылки в поле "url"')
+        raise InvalidAPIUsage(WRONG_FORMAL.format(field='url'))
     if 'custom_id' in data and data['custom_id'] not in ('', None):
         if not re.search(SHORT_LINK_REGEXP, data['custom_id']):
-            raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+            raise InvalidAPIUsage(WRONG_SHORTCUT_NAME)
         shortcut = data['custom_id']
         if is_shortcut_exists(shortcut):
-            raise InvalidAPIUsage(f'Имя "{shortcut}" уже занято.')
+            raise InvalidAPIUsage(NAME_TAKEN.format(shortcut=shortcut))
     else:
         shortcut = get_unique_short_id()
 
@@ -43,4 +50,4 @@ def get_original_link(short_id):
         url_map = URL_map.query.filter_by(short=short_id).first()
         if url_map:
             return jsonify({'url': url_map.original}), 200
-    raise InvalidAPIUsage('Указанный id не найден', 404)
+    raise InvalidAPIUsage(ID_NOT_FOUND, 404)
